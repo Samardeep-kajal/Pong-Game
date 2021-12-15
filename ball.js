2e5fe833d7af3af0a2256838d863e88e4fca591c
Samardeep kajal <samk712003@gmail.com>
2021-12-15 18:29:37 +0530

    Integreted paddle and game logic JavaScript

--- ball.js
+++ ball.js
@@ -40,7 +40,7 @@ export default class Ball {
     this.velocity = INITIAL_VELOCITY;
   }
 
-  update(delta) {
+  update(delta, paddleRects) {
     this.x += this.direction.x * this.velocity * delta;
     this.y += this.direction.y * this.velocity * delta;
     this.velocity += VELOCITY_INCREASE * delta;
@@ -50,13 +50,21 @@ export default class Ball {
       this.direction.y *= -1;
     }
 
-    if (rect.right >= window.innerWidth || rect.left <= 0) {
+    if (paddleRects.some((r) => isCollision(r, rect))) {
       this.direction.x *= -1;
     }
   }
-  //Need code fixing - continue from :10
 }
 
 function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min;
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  );
 }
